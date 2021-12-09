6e42aaf3e62d378b4258fcb471e6579c654cd55c
Deepak Sinha <deepak@Deepaks-MacBook-Pro.local>
2021-12-10 01:30:35 +0530

    created a database.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import movies as movies
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -10,11 +11,39 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+##CREATE DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+db.create_all()
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your rating for the movie?")
+    review = StringField("Your review")
+    submit = SubmitField("Done")
+
+@app.rooute("/edit", methods=['GET', 'POST'])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    if form.validate_on_submit()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    all_movies = Movie.query.all()
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
